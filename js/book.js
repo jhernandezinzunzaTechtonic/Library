6944a9434a340f35aa72ee426ec065fec4f467ab
Jose Hernandez-Inunza <>
2018-10-02 14:58:55 -0600

    playing around with add book function

--- js/book.js
+++ js/book.js
@@ -1,4 +1,4 @@
-function book (title){
+function book (title, author, numPages, pubDate){
   this.title = title;
   this.author = author;
   this.numberOfPages = numPages;
@@ -8,3 +8,13 @@ function book (title){
 book.prototype.editBook = function(oBook){
 
 };
+
+
+var testBook = new book("testBook", "Jose", 1, 2018);
+  testBook.title = "Hello World";
+  // testBook.author = "Jose";
+  // testBook.numberOfPages = 1;
+  // testBook.publishDate = 2018;
+
+
+  // document.getElementById("demo").innerHTML = testBook.title;
--- js/engine.js
+++ js/engine.js
@@ -2,15 +2,50 @@ function Library(){
   this.bookshelf = new Array();
 };
 
-Library.prototype.addBook = function (book) {
-
+// Add a book to my Library's bookshelf.
+// Return true if it is not already added, false if already added.
+Library.prototype.addBook = function (oBook) {
+  testLibrary.bookshelf.push(oBook);
+  console.log("You called the add Book function");
 };
+
+var testLibrary = new Library();
+var testBook = new book("testBook", "Jose", 1, 2018);
+var testBook2 = new book("testBook2", "Charles", 1, 2019);
+
+testLibrary.addBook(testBook);
+testLibrary.addBook(testBook2);
+console.log(testLibrary.bookshelf);
 
 Library.prototype.removeBookByTitle = function (title){
 
 }
 
+Library.prototype.removeBookByAuthor = function(authorName){
 
+}
+
+Library.prototype.getRandomBook = function(){
+
+}
+Library.prototype.getBookByTitle = function(title){
+
+}
+Library.prototype.getBookByAuthor = function(authorName){
+
+}
+Library.prototype.addBooks = function(books){
+
+}
+Library.prototype.getAuthors = function(){
+
+}
+Library.prototype.getRandomAuthor = function(){
+
+}
+
+
+// console.log(window.bookshelf);
 document.addEventListener("DOMContentLoaded", function(e){
   window.gLibrary = new Library();
 })
