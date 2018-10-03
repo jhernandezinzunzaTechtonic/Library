3973076859e4ec7a3d31333398f6823e9719e5dd
Jose Hernandez-Inunza <>
2018-10-03 10:23:28 -0600

    got search by author and title completed

--- js/book.js
+++ js/book.js
@@ -9,9 +9,18 @@ book.prototype.editBook = function(oBook){
 
 };
 
-var testBook = new book("testBook", "Jose", 1, 2018);
+var testBook1 = new book("testBook1", "Jose", 1, 2018);
 var testBook2 = new book("testBook2", "Charles", 1, 2019);
+var testBook3 = new book("testBook3", "John", 10, 2012);
+var testBook4 = new book("testBook4", "Eddie", 14, 2015);
+
+var testBookArray = [
+  {testBook1},
+  {testBook2},
+  {testBook3},
+  {testBook4}];
 
 document.getElementById("demo").innerHTML = "Current Books:" + "<br/>";
-document.getElementById("demo").innerHTML += testBook.title + "<br/>";
+document.getElementById("demo").innerHTML += testBook1.title + "<br/>";
 document.getElementById("demo").innerHTML += testBook2.title + "</br>";
+document.getElementById("demo").innerHTML += testBook3.title + "</br>";
--- js/engine.js
+++ js/engine.js
@@ -19,6 +19,9 @@ Library.prototype.addBook = function (oBook) {
   if (gLibrary.isInLibrary(oBook)) {
     console.log("Sorry, this book is already in our library");
     return false;
+  } else if (typeof oBook !== "object") {
+    console.log("Please enter in a book object");
+    return false;
   } else {
     this.bookshelf.push(oBook);
     console.log("You succesfully added a new Book!");
@@ -41,30 +44,59 @@ Library.prototype.removeBookByTitle = function (title){
   }
 }
 
+// Remove specific book or books by a certain author.
+// Return true if book is removed and false if no books match.
 Library.prototype.removeBookByAuthor = function(authorName){
   for (i=0; i < gLibrary.bookshelf.length; i++){
-    if (authorName === gLibrary.bookshelf[i]["author"]){
+    // console.log(authorName);
+    // console.log(gLibrary.bookshelf[i]["author"]);
+    if (authorName == gLibrary.bookshelf[i]["author"]){
       gLibrary.bookshelf.splice(i, 1);
       console.log("Books by " + "\""+authorName+"\"" + " successfully removed.");
       return true;
-    } else {
-      console.log("There is no book by " + "\""+authorName+"\"" + " in the library");
-      return false
     }
   }
+  console.log("There is no book by " + "\""+authorName+"\"" + " in the library");
+  return false;
 }
 
-Library.prototype.getRandomBook = function(){
-
+// Return a random book object from the bookshelf array.
+// Return a book object if you find a book, null if there are no books.
+Library.prototype.getRandomBook = function () {
+  var randomIndex = Math.floor(Math.random() * Math.floor(gLibrary.bookshelf.length));
+  return gLibrary.bookshelf[randomIndex];
 }
+
+// Return all books that completely or partially match the string title passed into the function.
+// Return array of book objects if you find books with matching titles, empty array if no books are found.
 Library.prototype.getBookByTitle = function(title){
+    var titleResults = gLibrary.bookshelf.filter(function(titlesSearched) {
+        return titlesSearched.title.indexOf(title) > -1;
+      });
+      console.log(titleResults);
+      return false;
+    }
 
-}
+// Find all books where the authors name partially or completely matches the authorName argument passed to the function.
+// Return array of books if you find books with matching authors, empty array if no books match.
 Library.prototype.getBookByAuthor = function(authorName){
-
+  var authorResults = gLibrary.bookshelf.filter(function(titlesSearched) {
+      return titlesSearched.title.indexOf(authorName) > -1;
+    });
+    console.log(authorResults);
+    return false;
 }
-Library.prototype.addBooks = function(books){
 
+// Takes multiple books, in the form of an array of book objects, and adds the objects to my bookshelf array.
+// Return number of books succesfully added, 0 is no books were added.
+Library.prototype.addBooks = function(booksArray){
+  if (booksArray.length >= 0) {
+    gLibrary.bookshelf.concat(booksArray);
+    console.log(booksArray.length + " Books Added");
+    return true;
+  } else if(booksArray !== undefined ){
+    console.log("Please enter in a proper array.");
+  }
 }
 Library.prototype.getAuthors = function(){
 
