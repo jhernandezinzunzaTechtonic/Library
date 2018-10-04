2d406e44a3c51d60b16f59b0ee80874f485e5737
Jose Hernandez-Inunza <>
2018-10-04 14:37:19 -0600

    fixed dit book bug

--- js/book.js
+++ js/book.js
@@ -9,8 +9,12 @@ function book (title, author, numPages, pubDate){
 // Takes in an object that can have some or all of the book properties to overrite original book properties.
 // Returns the edited book object.
 book.prototype.editBook = function(oBook){
-  this.title = oBook.title || this.title;
-  this.author = oBook.author || this.author;
-  this.numberOfPages = oBook.numberOfPages || this.numberOfPages;
-  this.publishDate = oBook.publishDate || this.publishDate;
+  if (gLibrary.isBookInLibrary(oBook)) {
+    console.log("This book already exists in the library.");
+  } else {
+    this.title = oBook.title || this.title;
+    this.author = oBook.author || this.author;
+    this.numberOfPages = oBook.numberOfPages || this.numberOfPages;
+    this.publishDate = oBook.publishDate || this.publishDate;
+  }
 };
